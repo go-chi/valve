f8c1264f81587e252604a16dfc3c2a1fdd1242bd
Peter Kieltyka <peter.kieltyka@pressly.com>
2017-07-05 18:13:38 -0400

    Move contextKey type to bottom of source file

--- valve.go
+++ valve.go
@@ -15,17 +15,6 @@ var (
 	ErrOff          = errors.New("valve: valve already shutdown")
 )
 
-// contextKey is a value for use with context.WithValue. It's used as
-// a pointer so it fits in an interface{} without allocation. This technique
-// for defining context keys was copied from Go 1.7's new use of context in net/http.
-type contextKey struct {
-	name string
-}
-
-func (k *contextKey) String() string {
-	return "valve context value " + k.name
-}
-
 type Valve struct {
 	stopCh chan struct{}
 	wg     sync.WaitGroup
@@ -153,4 +142,15 @@ func (v *Valve) ShutdownHandler(next http.Handler) http.Handler {
 		}
 	}
 	return http.HandlerFunc(fn)
+}
+
+// contextKey is a value for use with context.WithValue. It's used as
+// a pointer so it fits in an interface{} without allocation. This technique
+// for defining context keys was copied from Go 1.7's new use of context in net/http.
+type contextKey struct {
+	name string
+}
+
+func (k *contextKey) String() string {
+	return "valve context value " + k.name
 }
